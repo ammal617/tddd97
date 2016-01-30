c53c3d45ca10b8c22cc629d480a2ebeec5bbcf69
Alexander Lantz <alela654@li15-17.ida.liu.se>
2016-01-30 20:54:59 +0100

    lab1 almost done

--- twidder/js/client.js
+++ twidder/js/client.js
@@ -1,28 +1,39 @@
 displayViews = function(){
     
 };
+var errorMessage;
+var Repeatpsw;
+var userObject = {email:"", password:"", firstname:"", familyname:"", gender:"", city:"", country:""};
 
 window.onload = function(){
     var htmlDiv = document.getElementById('currentView');
     var htmlWelcome = document.getElementById('welcomeView');
-    
-    htmlDiv.innerHTML = htmlWelcome.innerHTML;
+    var htmlLogin = document.getElementById('loginView');
+    if(localStorage.getItem("userToken") == "nothing"){
+        htmlDiv.innerHTML = htmlWelcome.innerHTML;
+    }
+    else{
+        htmlDiv.innerHTML = htmlLogin.innerHTML;
+        loadUserInfo();
+    }
 };
+function loadUserInfo(){
+    var homeUserInfo = serverstub.getUserDataByToken(localStorage.getItem("userToken"));
+    console.log(homeUserInfo.data);
+    homeFirstName.innerHTML = homeUserInfo.data.firstname;
+    homeLastName.innerHTML = homeUserInfo.data.familyname;
+    homeEmail.innerHTML = homeUserInfo.data.email;
+    homeGender.innerHTML = homeUserInfo.data.gender;
+    homeCity.innerHTML = homeUserInfo.data.city;
+    homeCountry.innerHTML = homeUserInfo.data.country;
+}
 
 function checkBlanks(){
-    var firstName = document.forms["signUpForm"]["Firstname"].value;
-    var lastName = document.forms["signUpForm"]["Lastname"].value;
-    var City = document.forms["signUpForm"]["City"].value;
-    var Country = document.forms["signUpForm"]["Country"].value;
-    var Email = document.forms["signUpForm"]["Email"].value;
-    var Password = document.forms["signUpForm"]["Password"].value;
-    var Repeatpsw = document.forms["signUpForm"]["Repeatpsw"].value;
-    if(firstName == '' || lastName == '' || City == '' || Country == '' || Email == '' || Password == '' || Repeatpsw == ''){
-        console.log("you fucked up!");
+    if(userObject.firstname == "" || userObject.familyname == "" || userObject.city == "" || userObject.country == "" || userObject.email == "" || userObject.password == '' || Repeatpsw == ''){
+        errorMessage = "Missing information";
         return false;
     }
     else{
-        console.log("You did good");   
         return true;
     }
 }
@@ -31,7 +42,7 @@ function checkEmail(){
     var Email = document.forms["signUpForm"]["Email"].value;
     var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if(!re.test(Email)){
-        console.log("not valid email");
+        errorMessage = "Not valid email";
         return false;
     }
     else{
@@ -40,25 +51,81 @@ function checkEmail(){
 }
 
 function checkPassword(){
-    var Password = document.forms["signUpForm"]["Password"].value;
     var Repeatpsw = document.forms["signUpForm"]["Repeatpsw"].value;
     
-    if(Password.length < 8){
-        console.log("password too short");
+    if(userObject.password.length < 8){
+        errorMessage = "Password is to short";
         return false;
     }
-    else if(Password != Repeatpsw){
-        console.log("passwords don't match"); 
+    else if(userObject.password != Repeatpsw){
+        errorMessage = "Passwords don't match";
         return false;
     }
     else{
-        console.log("all good");   
         return true;
     }
 }
 
-function trySignIn(){
-    if(checkBlanks() || checkEmail() || checkPassword()){
-        console.log("success!")   
+function createUser(){
+    userObject.firstname = document.forms["signUpForm"]["Firstname"].value;
+    userObject.familyname = document.forms["signUpForm"]["Lastname"].value;
+    var genderList = document.getElementById("gendervalueID");
+    userObject.gender = genderList.options[genderList.selectedIndex].value;
+    userObject.city = document.forms["signUpForm"]["City"].value;
+    userObject.country = document.forms["signUpForm"]["Country"].value;
+    userObject.email = document.forms["signUpForm"]["Email"].value;
+    userObject.password = document.forms["signUpForm"]["Password"].value;
+    Repeatpsw = document.forms["signUpForm"]["Repeatpsw"].value;
+    if(checkPassword() && checkEmail() && checkBlanks()){
+        var errorDiv = document.getElementById('errorMessage');
+        errorDiv.innerHTML = "Success!";
+        document.getElementById("errorMessage").style.color = "green";
+        serverstub.signUp(userObject);
+        document.forms["signUpForm"].reset();
     }
+    else{
+        var errorDiv = document.getElementById('errorMessage');
+        errorDiv.innerHTML = errorMessage;
+    }
+
+}
+function tryloginUser(){
+    var loginUser = document.forms["loginForm"]["email"].value;
+    var loginPass = document.forms["loginForm"]["password"].value;
+    var serverResp = serverstub.signIn(loginUser, loginPass);
+
+    if(serverResp.success){
+        localStorage.setItem("userToken", serverResp.data);
+        location.reload();
+    }
+    else{
+        document.forms["loginForm"].reset();
+        var errorLoginDiv = document.getElementById('errorLoginMessage');
+        errorLoginDiv.innerHTML = "Wrong credentials";
+    }
+}
+function changeMyPassword(){
+    var oldPass = document.forms["changePass"]["oldpass"].value;
+    var newPass = document.forms["changePass"]["newpass"].value;
+    var repnewPass = document.forms["changePass"]["repnewpass"].value;
+    if (newPass == repnewPass){
+        var passrespons = serverstub.changePassword(localStorage.getItem("userToken"), oldPass, newPass);
+        if(passrespons.success){
+            console.log(passrespons.message);
+            document.forms["changePass"].reset();
+        }
+        else{
+            console.log(passrespons.message);
+            document.forms["changePass"].reset();
+        }
+    }
+    else{
+        console.log("Passwords don't match");
+        document.forms["changePass"].reset();
+    }
+}
+
+function logoutUser(){
+    localStorage.setItem("userToken", "nothing");
+    location.reload();
 }
