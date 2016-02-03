90c0c1eb7af1cd05f1faf3b80d790c9c8985e39c
Alexander Lantz <alela654@li15-9.ida.liu.se>
2016-02-03 15:10:24 +0100

    setup routes on server

--- lab2/server.py
+++ lab2/server.py
@@ -1,10 +1,33 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", method=['GET'])
 def hello():
     return "Hello World!"
-@app.route("/signIn/")
+
+@app.route("/signIn")
+def hola():
+        return "Hola worlda!"
+
+@app.route("/signOut")
+def hola():
+        return "Hola worlda!"
+
+@app.route("/changePass")
+def hola():
+        return "Hola worlda!"
+
+@app.route("/signIn")
+def hola():
+        return "Hola worlda!"
+
+@app.route("/signIn")
+def hola():
+        return "Hola worlda!"
+
+@app.route("/signIn")
+def hola():
+        return "Hola worlda!"
 
 if __name__ == "__main__":
     app.run()
