8795d23ecff06c0cbb2c0d1095646cd992ddb1a8
Alexander Lantz <alela654@li15-16.ida.liu.se>
2016-02-04 15:46:59 +0100

    added server functions

--- lab2/server.py
+++ lab2/server.py
@@ -5,27 +5,52 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
-@app.route("/signIn")
+@app.route("/sign_in", method=['POST'])
+def sign_in():
+        email = request.form['email']
+        password = request.form['password']
+        return
+
+@app.route("/sign_out", method=['POST'])
+def hola():
+        return
+
+@app.route("/change_password", method=['POST'])
+def change_password():
+    old_password = request.form['password']
+    new_password = request.form['new_password']
+        return
+
+@app.route("/sign_up", method=['POST'])
+def hola():
+
+        email = request.form['email']
+        password = request.form['password']
+        First_name = request.form['First_name']
+        Family_name = request.form['Family_name']
+        gender = request.form['Gender']
+        city = request.form['city']
+        country = request.form['country']
+
+        return
+
+@app.route("/get_user_data_by_token/<token>", method=['GET'])
 def hola():
         return "Hola worlda!"
 
-@app.route("/signOut")
+@app.route("/get_user_data_by_email/<token>/<email>", method=['GET'])
 def hola():
         return "Hola worlda!"
 
-@app.route("/changePass")
+@app.route("/get_user_message_by_token/<token>", method=['GET'])
 def hola():
         return "Hola worlda!"
 
-@app.route("/signIn")
+@app.route("/get_user_message_by_email/<token>/<email>", method=['GET'])
 def hola():
-        return "Hola worlda!"
+        return
 
-@app.route("/signIn")
-def hola():
-        return "Hola worlda!"
-
-@app.route("/signIn")
+@app.route("/post_message", method=['post'])
 def hola():
         return "Hola worlda!"
 
